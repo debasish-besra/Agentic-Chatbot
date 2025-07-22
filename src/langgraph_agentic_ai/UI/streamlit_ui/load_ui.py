0073053c36804cbd7ee128ee126182bb80c9a545
debasish-besra <debasish.besra.ai@gmail.com>
2025-07-22 11:56:39 +0530

    configured the load_ui.py file

--- src/langgraph_agentic_ai/UI/streamlit_ui/load_ui.py
+++ src/langgraph_agentic_ai/UI/streamlit_ui/load_ui.py
@@ -33,6 +33,15 @@ class LoadStreamlitUI:
             ## Usecase selection
             self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)
 
+            if self.user_controls["selected_usecase"] == "Chatbot with Web Search":
+                self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"] = st.text_input("TAVILY_API_KEY", type='password')
+
+                # Validate API key
+                if not self.user_controls["TAVILY_API_KEY"]:
+                    st.warning("⚠️ Please enter your TAVILY API KEY to proceed. Don't have? refer : https://app.tavily.com/home")
+
+
+
         return self.user_controls
     
     
