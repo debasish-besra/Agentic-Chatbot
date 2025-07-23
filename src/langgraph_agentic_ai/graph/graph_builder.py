8832e4021671119b89aa82474b1deabc18bdcde3
debasish-besra <debasish.besra.ai@gmail.com>
2025-07-23 10:50:49 +0530

    AI News Summarizer Functionality created

--- src/langgraph_agentic_ai/graph/graph_builder.py
+++ src/langgraph_agentic_ai/graph/graph_builder.py
@@ -5,7 +5,7 @@ from src.langgraph_agentic_ai.nodes.basic_chatbot_node import BasicChatbotNode
 from src.langgraph_agentic_ai.tools.search_tool import get_tools,create_tool_node
 from langgraph.prebuilt import tools_condition,ToolNode
 from src.langgraph_agentic_ai.nodes.chatbot_with_Tool_node import ChatbotWithToolNode
-
+from src.langgraph_agentic_ai.nodes.ai_news_node import AINewsNode
 
 class GraphBuilder:
     def __init__(self,model):
@@ -54,6 +54,22 @@ class GraphBuilder:
         self.graph_builder.add_edge("tools","chatbot")
        
 
+    def ai_news_builder_graph(self):
+        ai_news_node=AINewsNode(self.llm)
+
+         ## added the nodes
+
+        self.graph_builder.add_node("fetch_news", ai_news_node.fetch_news)
+        self.graph_builder.add_node("summarize_news", ai_news_node.summarize_news)
+        self.graph_builder.add_node("save_result", ai_news_node.save_result)
+
+        #added the edges
+
+        self.graph_builder.set_entry_point("fetch_news")
+        self.graph_builder.add_edge("fetch_news","summarize_news")
+        self.graph_builder.add_edge("summarize_news","save_result")
+        self.graph_builder.add_edge("save_result", END)
+
 
 
     def setup_graph(self, usecase: str):
@@ -64,6 +80,8 @@ class GraphBuilder:
             self.basic_chatbot_build_graph()
         if usecase == "Chatbot With Web":
             self.chatbot_with_tools_build_graph()
+        if usecase == "AI News":
+            self.ai_news_builder_graph()
 
         return self.graph_builder.compile()
 
