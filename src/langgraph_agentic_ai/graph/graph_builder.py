eddb936d88685cd02dbcac6395ab3ad246a419a6
debasish-besra <debasish.besra.ai@gmail.com>
2025-07-23 06:06:00 +0530

    Implemented new node - chatbot_with_web_tool_node

--- src/langgraph_agentic_ai/graph/graph_builder.py
+++ src/langgraph_agentic_ai/graph/graph_builder.py
@@ -4,6 +4,7 @@ from langgraph.graph import START,END
 from src.langgraph_agentic_ai.nodes.basic_chatbot_node import BasicChatbotNode
 from src.langgraph_agentic_ai.tools.search_tool import get_tools, create_tool_node
 from langgraph.prebuilt import tools_condition, ToolNode
+from src.langgraph_agentic_ai.nodes.chatbot_with_web_tool_node import ChatbotWithWebToolNode
 
 
 class GraphBuilder:
@@ -41,10 +42,11 @@ class GraphBuilder:
         llm = self.llm
 
         # Define the chatbot node
-
+        obj_chatbot_with_node = ChatbotWithWebToolNode(llm)
+        chatbot_node = obj_chatbot_with_node(tools)
 
         # Add nodes
-        self.graph_builder.add_node("chatbot", "")
+        self.graph_builder.add_node("chatbot", chatbot_node)
         self.graph_builder.add_node("tools", tool_node)
 
         # Define conditional and direct edges
